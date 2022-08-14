a7f365078f34e35177ce92a8075b8b32b223232b
Michelsgab <eugabmichels@gmail.com>
2022-08-14 20:41:01 -0300

    Tela de pesquisa dinâmica

--- src/app/pages/pesquisa/pesquisa.component.ts
+++ src/app/pages/pesquisa/pesquisa.component.ts
@@ -1,14 +1,25 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { IFuncionario } from 'src/app/Funcionario';
+import { ProjetoService } from 'src/app/service/projeto.service';
 
 @Component({
   selector: 'app-pesquisa',
   templateUrl: './pesquisa.component.html',
-  styleUrls: ['./pesquisa.component.css']
+  styleUrls: ['./pesquisa.component.css'],
 })
 export class PesquisaComponent implements OnInit {
-  constructor() { }
+  funcionarios: Array<IFuncionario> = [];
 
-  ngOnInit(): void {
+  constructor(private projetoService: ProjetoService, private router: Router) {
+    this.getFuncionariosId();
   }
 
+  ngOnInit(): void {}
+
+  getFuncionariosId(): void {
+    this.projetoService
+      .getAll()
+      .subscribe((funcionario) => (this.funcionarios = funcionario));
+  }
 }
